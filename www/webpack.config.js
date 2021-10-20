6d0d152ba6079f7c93c80c9f3358f9fcfd4189f1
juancortelezzi <juanbicortelezzi@gmail.com>
2021-10-20 00:23:42 -0300

    added frontend

---
+++ www/webpack.config.js
@@ -0,0 +1,31 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
+  mode: "development",
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./src/index.html", to: "." }],
+    }),
+  ],
+};
